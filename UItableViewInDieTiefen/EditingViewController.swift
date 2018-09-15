37a7758f9009bf7da0135a49994ee66ed0fbc462
stenlee <lidan1811@gmail.com>
2018-09-15 22:52:28 +0200

    用"func moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath”语句完成DetailView内容的可置更改功能。

---
+++ UItableViewInDieTiefen/EditingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EditingViewController.swift
+//  UItableViewInDieTiefen
+//
+//  Created by Dan Li on 15.09.18.
+//  Copyright © 2018 DanStenLee. All rights reserved.
+//
+
+import UIKit
+
+class EditingViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var textField: UITextView!
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
