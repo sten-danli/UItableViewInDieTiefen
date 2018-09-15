358e9147a4e5184c911f9866e69185fb2a8a87d5
stenlee <lidan1811@gmail.com>
2018-09-15 22:10:41 +0200

    Add Edit in DetaiView

---
+++ UItableViewInDieTiefen/saveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  saveViewController.swift
+//  UItableViewInDieTiefen
+//
+//  Created by Dan Li on 15.09.18.
+//  Copyright © 2018 DanStenLee. All rights reserved.
+//
+
+import UIKit
+
+class saveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
